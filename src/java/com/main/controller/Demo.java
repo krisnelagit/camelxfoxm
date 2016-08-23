b01ca7d1bc6b1e552d98b5fe1047001d540fbfc7
nityanand <nityanand@NityanandPc>
2016-08-23 10:43:04 +0530

    latest changes:all regards nityanand

--- src/java/com/main/controller/Demo.java
+++ src/java/com/main/controller/Demo.java
@@ -5,20 +5,53 @@
  */
 package com.main.controller;
 
-import com.main.mailer.KrisnelaSendMailAPI;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Demo {
 
-    public static void main(String[] args) throws Exception {
-        String configfile="dispatcher-servlet.xml";
-        ConfigurableApplicationContext context=new ClassPathXmlApplicationContext(configfile);
-        KrisnelaSendMailAPI aPI=(KrisnelaSendMailAPI)context.getBean("krisnelaEmail");
-        String to="krisnelatest@gmail.com";
-        String from="krisnelatest@gmail.com";
-        String subject="test subject";
-        String body="There you go.. You got an email.. Let's understand details on how Spring MVC works -- By Krisnela Admin\"";
-        aPI.sendmailinfok(from, to, subject, body);
+    private static final long MEGABYTE = 1024L * 1024L;
+
+    public static long bytesToMegabytes(long bytes) {
+        return bytes / MEGABYTE;
+    }
+
+    public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+        BufferedReader br = null;
+        try {
+            String scurrentLine;
+            br = new BufferedReader(new FileReader("/home/nityanand/Desktop/testt.txt"));
+            while ((scurrentLine = br.readLine()) != null) {
+//                System.out.print(scurrentLine);   
+                List<String> list = Arrays.asList(scurrentLine.split(" "));
+                Set<String> uniquewords = new HashSet<String>(list);
+                for (String word : uniquewords) {
+                    System.out.println(word + ": " + Collections.frequency(list, word));
+                }
+            }
+            
+
+        } catch (Exception e) {
+        }
+        long stopTime = System.currentTimeMillis();
+        long elapsedTime = stopTime - startTime;
+        System.out.println(elapsedTime);
+
+// Get the Java runtime
+        Runtime runtime = Runtime.getRuntime();
+// Run the garbage collector
+        runtime.gc();
+// Calculate the used memory
+        long memory = runtime.totalMemory() - runtime.freeMemory();
+        System.out.println("Used memory is bytes: " + memory);
+        System.out.println("Used memory is megabytes: "
+                + bytesToMegabytes(memory));
     }
 }
